3e7dab0cea65eb9da997f06c5c64935ee88b1f9b
unknown <ivandelucca123@gmail.com>
2025-08-01 13:35:41 -0300

    Fixing home page link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     return(
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to='/'>Movie App</Link>
+                <Link to='/MoviesList'>Movie App</Link>
             </div>
             <div className="navbar-links">
                 <Link to='/MoviesList' className="nav-link">Home</Link>
